515ff031470bcdd71802b584a9142c8e5d819648
guptaaryan1 <acgupta6@gmail.com>
2024-11-11 15:38:25 -0600

    Frontend with placeholders

--- CS124H React Frontend/src/App.jsx
+++ CS124H React Frontend/src/App.jsx
@@ -1,34 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import GameInterface from './components/GameInterface'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Categories from './components/Categories.jsx';
+import Difficulty from './components/Difficulty.jsx';
+import Game from './components/Game.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<GameInterface />} />
+                    <Route path="/categories" element={<Categories />} />
+                    <Route path="/difficulty" element={<Difficulty />} />
+                    <Route path="/game" element={<Game />} />
+                </Routes>
+            </div>
+        </Router>
   )
 }
 
